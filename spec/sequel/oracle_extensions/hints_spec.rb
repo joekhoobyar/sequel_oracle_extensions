afb762ed0958c7ab73162d6c6bbc6e8cf38abe93
Joe Khoobyar <joe@ankhcraft.com>
2010-05-24 17:53:11 -0400

    finished spec examples for hints extension

--- spec/sequel/oracle_extensions/hints_spec.rb
+++ spec/sequel/oracle_extensions/hints_spec.rb
@@ -66,84 +66,92 @@ describe "Sequel::OracleExtensions::Hints" do
   end
 
   share_examples_for "clause-specific callspec" do
-	  it "callspec (String) applies clause hints" do
+	  it "callspec (String) applies hints" do
 	    apply_hints! @hints.first
       hints_to_check(@new_ds, @clause, @hints[0,1]).should == @hints[0,1]
 	  end
-	  it "callspec (String, ...) applies clause hints" do
+	  it "callspec (String, ...) applies hints" do
 	    apply_hints! *@hints
       hints_to_check(@new_ds, @clause, @hints).should == @hints
 	  end
   end
   
-  describe "appends clause-specific hints" do
+  describe "hints" do
     before(:each){ @ds, @hints = @db[:dual], ['foo', 'bar'] }
     
-    def hints_to_check(ds, type, input)
-      ds.opts[:hints][type][(@orig_hints[type].length rescue 0), input.length]
+    COMMON_GROUP_BODY = Proc.new do |group,name|
+      group.class_eval do
+			  describe "##{name}" do
+		      before(:each){ @method = :"#{name}" }
+			    it_should_behave_like "dataset cloning"
+			    it_should_behave_like "standard callspec"
+			  end
+			  describe "##{name}!" do
+		      before(:each){ @method = :"#{name}!" }
+			    it_should_behave_like "dataset modifying"
+			    it_should_behave_like "standard callspec"
+			  end
+			  TYPES.each do |clause|
+				  describe "##{clause}_#{name}" do
+			      before(:each){ @clause, @method = clause, :"#{clause}_#{name}"}
+				    it_should_behave_like "dataset cloning"
+				    it_should_behave_like "clause-specific callspec"
+				  end
+				  describe "##{clause}_#{name}!" do
+			      before(:each){ @clause, @method = clause, :"#{clause}_#{name}!"}
+				    it_should_behave_like "dataset modifying"
+				    it_should_behave_like "clause-specific callspec"
+				  end
+			  end
+		  end
     end
-
-	  describe "#hint" do
-      before(:each){ @method = :hint }
-	    it_should_behave_like "dataset cloning"
-	    it_should_behave_like "standard callspec"
-	  end
-	
-	  describe "#hint!" do
-      before(:each){ @method = :hint! }
-	    it_should_behave_like "dataset modifying"
-	    it_should_behave_like "standard callspec"
+	    
+	  describe "adding hints" do
+	    def hints_to_check(ds, type, input)
+	      ds.opts[:hints][type][(@orig_hints[type].length rescue 0), input.length]
+	    end
+	    COMMON_GROUP_BODY.call self, :hint
 	  end
 	  
-	  TYPES.each do |clause|
-
-		  describe "##{clause}_hint" do
-	      before(:each){ @clause, @method = clause, :"#{clause}_hint"}
-		    it_should_behave_like "dataset cloning"
-		    it_should_behave_like "clause-specific callspec"
-		  end
-		
-		  describe "##{clause}_hint!" do
-	      before(:each){ @clause, @method = clause, :"#{clause}_hint!"}
-		    it_should_behave_like "dataset modifying"
-		    it_should_behave_like "clause-specific callspec"
-		  end
+	  describe "overwriting hints" do
+	    def hints_to_check(ds, type, input)
+	      ds.opts[:hints][type]
+	    end
+	    COMMON_GROUP_BODY.call self, :hints
 	  end
 	  
+    describe "#hint_sql" do
+	    it "generates clause-specific hint SQL" do
+	      # set them all up front so we can test whether they get mixed up.
+        TYPES.each do |type| @ds.hints! type, "hint for #{type}" end
+	      TYPES.each do |type|
+	        sql = (clause = type.to_s.upcase).dup
+	        @ds.hint_sql(type, sql).should equal(sql)
+	        sql.should == "#{clause} /*+ hint for #{type} */"
+	      end
+	    end
+	    
+	    it "skips empty hints" do
+	      TYPES.each do |type|
+	        sql = (clause = type.to_s.upcase).dup
+	        @ds.hint_sql(type, sql).should be_nil
+	        sql.should == clause
+	      end
+	    end
+	    
+      TYPES.each do |type|
+		    it "is called by ##{type}_hint_sql" do
+	        clause = type.to_s.upcase
+		      @ds.should_receive(:hint_sql).with(type, clause)
+		      @ds.__send__ :"#{type}_hint_sql", clause
+		    end
+		    it "is called by ##{type}_sql" do
+	        args, clause = [], type.to_s.upcase
+		      @ds.should_receive(:hint_sql).with(type, clause)
+		      args.push :dual, :dual, :x if type == :merge
+		      @ds.__send__ :"#{type}_sql", *args
+		    end
+	    end
+    end
   end
-  
-  describe "sets clause-specific hints" do
-    before(:each){ @ds, @hints = @db[:dual], ['foo', 'bar'] }
-    
-    def hints_to_check(ds, type, input)
-      ds.opts[:hints][type]
-    end
-
-    describe "#hints" do
-      before(:each){ @method = :hints }
-      it_should_behave_like "dataset cloning"
-      it_should_behave_like "standard callspec"
-    end
-
-    describe "#hints!" do
-      before(:each){ @method = :hints! }
-      it_should_behave_like "dataset modifying"
-      it_should_behave_like "standard callspec"
-    end
-    
-	  TYPES.each do |clause|
-      describe "##{clause}_hints" do
-	      before(:each){ @clause, @method = clause, :"#{clause}_hints"}
-        it_should_behave_like "dataset cloning"
-        it_should_behave_like "clause-specific callspec"
-      end
-    
-      describe "##{clause}_hints!" do
-	      before(:each){ @clause, @method = clause, :"#{clause}_hints!"}
-        it_should_behave_like "dataset modifying"
-        it_should_behave_like "clause-specific callspec"
-      end
-    end
-  end
-
 end
