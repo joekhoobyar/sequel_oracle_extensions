501d7dfc77268a051b74be2be4d7d790c93800d9
Joe Khoobyar <joe@ankhcraft.com>
2010-05-24 17:15:12 -0400

    more spec examples

--- spec/sequel/oracle_extensions/hints_spec.rb
+++ spec/sequel/oracle_extensions/hints_spec.rb
@@ -3,21 +3,31 @@ require 'sequel/oracle_extensions/merge'
 require 'sequel/oracle_extensions/hints'
 
 describe "Sequel::OracleExtensions::Hints" do
+
+  CLAUSES = %w(SELECT INSERT UPDATE DELETE MERGE)
+  TYPES = CLAUSES.map{|clause| clause.downcase.intern}
+
   before(:all) do
     @db = Sequel.connect(DATABASE_URL)
-    @clauses = %w(SELECT INSERT UPDATE DELETE MERGE)
-    @types = @clauses.map{|clause| clause.downcase.intern}
   end
 
+	def apply_hints!(*args)
+	  @old_hints = @ds.opts[:hints]
+	  @new_ds    = @ds.__send__(@method, *args)
+	
+	  @new_ds.should be_kind_of(Sequel::Dataset)
+	  @new_ds.opts[:hints].should_not be_empty
+	end
+  
   it "hooks into dataset clause methods" do
     [Sequel::Dataset, Sequel::Oracle::DatasetMethods].each do |klass|
-	    @clauses.each do |clause|
+	    CLAUSES.each do |clause|
 	      next unless klass.const_defined?(k = :"#{clause}_CLAUSE_METHODS")
 		    klass.const_get(k).first.should == "#{clause.downcase}_hint_sql".intern
 		  end
 	  end
   end
-  
+
   share_examples_for "dataset modifying" do
     after(:each) do
 		  @ds.should equal(@new_ds)
@@ -33,33 +43,36 @@ describe "Sequel::OracleExtensions::Hints" do
   end
   
   share_examples_for "standard callspec" do
-    def apply_hints!(*args)
-      @old_hints = @ds.opts[:hints]
-      @new_ds    = @ds.__send__(@method, *args)
-
-      @new_ds.should be_kind_of(Sequel::Dataset)
-      @new_ds.opts[:hints].should_not be_empty
-    end
-
-	  it "1 argument form (String) applies :select hints" do
+	  it "callspec (String) applies :select hints" do
 	    apply_hints! @hints.first
       hints_to_check(@new_ds, :select, @hints[0,1]).should == @hints[0,1]
 	  end
-	  it "1+ argument form (String, ...) applies :select hints" do
+	  it "callspec (String, ...) applies :select hints" do
 	    apply_hints! *@hints
       hints_to_check(@new_ds, :select, @hints).should == @hints
 	  end
-	  it "2 argument form (:clause, String)" do
-	    @types.each do |type|
+	  it "callspec (clause, String) applies clause hints" do
+	    TYPES.each do |type|
         apply_hints! type, @hints.first
 	      hints_to_check(@new_ds, type, @hints[0,1]).should == @hints[0,1]
 	    end
 	  end
-	  it "2+ argument form (:clause, String, ...)" do
-	    @types.each do |type|
+	  it "callspec (clause, String, ...) applies clause hints" do
+	    TYPES.each do |type|
         apply_hints! type, *@hints
 	      hints_to_check(@new_ds, type, @hints).should == @hints
 	    end
+	  end
+  end
+
+  share_examples_for "clause-specific callspec" do
+	  it "callspec (String) applies clause hints" do
+	    apply_hints! @hints.first
+      hints_to_check(@new_ds, @clause, @hints[0,1]).should == @hints[0,1]
+	  end
+	  it "callspec (String, ...) applies clause hints" do
+	    apply_hints! *@hints
+      hints_to_check(@new_ds, @clause, @hints).should == @hints
 	  end
   end
   
@@ -71,16 +84,32 @@ describe "Sequel::OracleExtensions::Hints" do
     end
 
 	  describe "#hint" do
+      before(:each){ @method = :hint }
 	    it_should_behave_like "dataset cloning"
 	    it_should_behave_like "standard callspec"
-      before(:each){ @method = :hint }
 	  end
 	
 	  describe "#hint!" do
+      before(:each){ @method = :hint! }
 	    it_should_behave_like "dataset modifying"
 	    it_should_behave_like "standard callspec"
-      before(:each){ @method = :hint! }
 	  end
+	  
+	  TYPES.each do |clause|
+
+		  describe "##{clause}_hint" do
+	      before(:each){ @clause, @method = clause, :"#{clause}_hint"}
+		    it_should_behave_like "dataset cloning"
+		    it_should_behave_like "clause-specific callspec"
+		  end
+		
+		  describe "##{clause}_hint!" do
+	      before(:each){ @clause, @method = clause, :"#{clause}_hint!"}
+		    it_should_behave_like "dataset modifying"
+		    it_should_behave_like "clause-specific callspec"
+		  end
+	  end
+	  
   end
   
   describe "sets clause-specific hints" do
@@ -91,15 +120,29 @@ describe "Sequel::OracleExtensions::Hints" do
     end
 
     describe "#hints" do
+      before(:each){ @method = :hints }
       it_should_behave_like "dataset cloning"
       it_should_behave_like "standard callspec"
-      before(:each){ @method = :hints }
     end
 
     describe "#hints!" do
+      before(:each){ @method = :hints! }
       it_should_behave_like "dataset modifying"
       it_should_behave_like "standard callspec"
-      before(:each){ @method = :hints! }
+    end
+    
+	  TYPES.each do |clause|
+      describe "##{clause}_hints" do
+	      before(:each){ @clause, @method = clause, :"#{clause}_hints"}
+        it_should_behave_like "dataset cloning"
+        it_should_behave_like "clause-specific callspec"
+      end
+    
+      describe "##{clause}_hints!" do
+	      before(:each){ @clause, @method = clause, :"#{clause}_hints!"}
+        it_should_behave_like "dataset modifying"
+        it_should_behave_like "clause-specific callspec"
+      end
     end
   end
 
